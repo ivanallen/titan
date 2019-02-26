bf0e89d9e121994f57d1e9d158db4725d2ce57ce
ivanallen <1007729991@qq.com>
2019-02-26 12:56:45 +0800

    SSE:server-sent events

--- main.go
+++ main.go
@@ -68,6 +68,50 @@ func main() {
 		})
 		multipartWriter.Close()
 	})
+
+	r.POST("/crawlimage2", func(c *gin.Context) {
+		var opts crawler.CrawlImageOption
+		if err := c.ShouldBindJSON(&opts); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		log.Printf(`request:{"seed":"%v","pattern":"%v"}`, opts.Seed, opts.Pattern)
+		/*
+			opts := &CrawlImageOption{
+				Seed:    "https://logo-tank.net",
+				Pattern: `http://logo-tank\.net(\?page=.*)?$`,
+			}
+		*/
+		c.Writer.Header().Set("Content-Type", "text/event-stream")
+		c.Writer.Header().Set("Cache-Control", "no-cache")
+		c.Writer.WriteHeader(http.StatusOK)
+
+		if f, ok := c.Writer.(http.Flusher); ok {
+			f.Flush()
+		} else {
+			log.Print("Damn, no flush")
+		}
+
+		crawlImage(&opts, func(imgInfo *crawler.ImageInfo, index int32, total int32) error {
+			report := fmt.Sprintf(`{"img":"%s", "from":"%s", "index":%d, "total":%d}`, imgInfo.ImageURL, imgInfo.FromURL, index, total)
+
+			// Write 可能不会完全写出。需要 Writen
+			c.Writer.Write([]byte("event: progress\n"))
+			c.Writer.Write([]byte("data: " + report + "\n\n"))
+
+			log.Printf("report:%v", report)
+
+			if f, ok := c.Writer.(http.Flusher); ok {
+				f.Flush()
+			} else {
+				log.Print("Damn, no flush")
+			}
+			return nil
+		})
+
+		c.Writer.Write([]byte("event: end\n"))
+		c.Writer.Write([]byte(`data: {"download_url:"/resource/tmp.zip"}` + "\n\n"))
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
 
